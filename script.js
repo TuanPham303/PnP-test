55cc858bdfb37ac2722a45b9bbc51c3d44e14280
TuanPham303 <phamcongtuan303@gmail.com>
2017-11-06 18:11:29 -0800

    add glowing effect on letter

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function runStripe(){
     stripe.classList.add('stripe-path');
   }
 }
+
+var mX, mY, distance,
+$element  = $('#letter-svg');
+
+function calculateDistance(elem, mouseX, mouseY) {
+  return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+}
+$(document).mousemove(function(e) {  
+  mX = e.pageX;
+  mY = e.pageY;
+  distance = calculateDistance($element, mX, mY);
+  if((1000 - distance)/1000 > 0.5){
+    $element.css('filter', `drop-shadow(rgba(255, 255, 255, ${(1000 - distance)/1000}) 0px 0px 6px) drop-shadow(rgba(255, 255, 255, ${(1000 - distance)/1000}) 0px 0px 6px) drop-shadow(rgba(255, 255, 255, ${(1000 - distance)/1000}) 0px 0px 6px)`);  
+  } else {
+    $element.css('filter', `drop-shadow(rgba(255, 255, 255, 0) 0px 0px 6px) drop-shadow(rgba(255, 255, 255, 0) 0px 0px 6px) drop-shadow(rgba(255, 255, 255, 0) 0px 0px 6px)`); 
+  }
+}); 
